2363bd342dfd8b56901b2627e2fdf82df548ad1a
Thierry Nischelwitzer <nischi@nischi.ch>
2020-04-04 07:41:59 +0200

    1 add first example for user api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    SequelizeModule.forRoot({
+      dialect: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "VersusVirus",
+      database: "schooling-tool",
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
